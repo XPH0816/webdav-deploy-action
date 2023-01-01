93ecbdf8e7b2f3f2de3e49c4421ec4d157b323e2
Lim Shi Song <50739818+XPH0816@users.noreply.github.com>
2023-01-01 21:59:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,12 @@ vendor = other
 " > ~/.config/rclone/rclone.conf
 
 if [ -z ${INPUT_EXCLUDE+x} ]; then 
-  rclone -v sync $INPUT_LOCAL woocart:$INPUT_REMOTE --exclude-from=/rclone-filters.txt
+  rclone sync $INPUT_LOCAL woocart:$INPUT_REMOTE --exclude-from=/rclone-filters.txt
 
 else 
   if [[ "$(declare -p $INPUT_EXCLUDE)" =~ "declare -a" ]]; then
-    rclone -v sync --exclude "'$INPUT_EXCLUDE[*]'" woocart:$INPUT_REMOTE
+    rclone sync --exclude "'$INPUT_EXCLUDE[*]'" woocart:$INPUT_REMOTE
   else
-    rclone -v sync --exclude $INPUT_EXCLUDE $INPUT_LOCAL woocart:$INPUT_REMOTE
+    rclone sync --exclude $INPUT_EXCLUDE $INPUT_LOCAL woocart:$INPUT_REMOTE
   fi 
 fi
