54aad2b0ee7680640d7d2b271d68d9173d71ca8e
Lim Shi Song <limshisong123@gmail.com>
2023-01-01 21:24:32 +0800

    Back to origin

--- Dockerfile
+++ Dockerfile
@@ -6,23 +6,10 @@ LABEL "com.github.actions.icon"="upload-cloud"
 LABEL "com.github.actions.color"="blue"
 LABEL "maintainer"="Niteo.co <info@niteo.co>"
 
-# User ID of share owner
-ENV OWNER=0
-
-# Location of directory where to mount the drive into the container.
-ENV WEBDRIVE_MOUNT=~/mnt/webdrive
-
-RUN apk --no-cache add ca-certificates davfs2 tini rsync bash
-
-COPY *.sh /usr/local/bin/
+RUN apk add --no-cache bash curl unzip ca-certificates fuse openssh-client jq \
+  && wget -qO- https://rclone.org/install.sh | bash \
+  && apk del curl unzip
 
 COPY entrypoint.sh /entrypoint.sh
 
-# ENTRYPOINT ["/entrypoint.sh"]
-
-# The default is to perform all system-level mounting as part of the entrypoint
-# to then have a command that will keep listing the files under the main share.
-# Listing the files will keep the share active and avoid that the remote server
-# closes the connection.
-ENTRYPOINT [ "tini", "-g", "--", "/usr/local/bin/entrypoint.sh" ]
-CMD [ "copy.sh" ]
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,66 +1,6 @@
 #!/usr/bin/env bash
 # Where are we going to mount the remote webdav resource in our container.
 
-DEST=${WEBDRIVE_MOUNT:-/mnt/webdrive}
-
-<<<<<<< HEAD
-# Check variables and defaults
-if [ -z "${INPUT_URL}" ]; then
-    echo "No URL specified!"
-    exit
-fi
-if [ -z "${INPUT_USERNAME}" ]; then
-    echo "No username specified, is this on purpose?"
-fi
-if [ -n "${WEBDRIVE_PASSWORD_FILE}" ]; then
-    WEBDRIVE_PASSWORD=$(read ${WEBDRIVE_PASSWORD_FILE})
-fi
-if [ -z "${INPUT_PASSWORD}" ]; then
-    echo "No password specified, is this on purpose?"
-fi
-
-# Create secrets file and forget about the password once done (this will have
-# proper effects when the PASSWORD_FILE-version of the setting is used)
-echo "$DEST $INPUT_USERNAME $INPUT_PASSWORD" >> /etc/davfs2/secrets
-unset INPUT_PASSWORD
-
-# Add davfs2 options out of all the environment variables starting with DAVFS2_
-# at the end of the configuration file. Nothing is done to check that these are
-# valid davfs2 options, use at your own risk.
-if [ -n "$(env | grep "DAVFS2_")" ]; then
-    echo "" >> /etc/davfs2/davfs2.conf
-    echo "[$DEST]" >> /etc/davfs2/davfs2.conf
-    for VAR in $(env); do
-        if [ -n "$(echo "$VAR" | grep -E '^DAVFS2_')" ]; then
-            OPT_NAME=$(echo "$VAR" | sed -r "s/DAVFS2_([^=]*)=.*/\1/g" | tr '[:upper:]' '[:lower:]')
-            VAR_FULL_NAME=$(echo "$VAR" | sed -r "s/([^=]*)=.*/\1/g")
-            VAL=$(eval echo \$$VAR_FULL_NAME)
-            echo "$OPT_NAME $VAL" >> /etc/davfs2/davfs2.conf
-        fi
-    done
-fi
-
-# Create destination directory if it does not exist.
-if [ ! -d $DEST ]; then
-    mkdir -p $DEST
-fi
-
-# Deal with ownership
-if [ $OWNER -gt 0 ]; then
-    adduser webdrive -u $OWNER -D -G users
-    chown webdrive $DEST
-fi
-
-# Mount and verify that something is present. davfs2 always creates a lost+found
-# sub-directory, so we can use the presence of some file/dir as a marker to
-# detect that mounting was a success. Execute the command on success.
-mount -t davfs $INPUT_URL $DEST -o uid=$OWNER,gid=users,dir_mode=755,file_mode=755
-if [ -n "$(ls -1A $DEST)" ]; then
-    echo "Mounted $INPUT_URL onto $DEST"
-    exec "$@"
-else
-    echo "Nothing found in $DEST, giving up!"
-=======
 mkdir -p ~/.config/rclone/
 echo "
 [woocart]
@@ -79,5 +19,4 @@ else
   else
     rclone -v sync --exclude $INPUT_EXCLUDE $INPUT_LOCAL woocart:$INPUT_REMOTE
   fi 
->>>>>>> parent of 3204814 (Update entrypoint.sh)
 fi
